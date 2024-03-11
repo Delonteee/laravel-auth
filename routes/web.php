1d64e324ea4755265a5ecf7bbbc854f1171fd2c1
Delonteee <davide.gasparotti@tutanota.com>
2024-03-11 16:09:29 +0100

    Aggiornato template con AUTH

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\MainController;
+use App\Http\Controllers\Admin\MainController as AdminMainController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,41 +17,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    $firstName = 'Gino';
-    $lastName = 'Paoli';
+Route::get('/', [MainController::class, 'index'])->name('home');
 
-    /*
-        compact: crea un array associativo le cui chiavi sono le stringhe
-                 che mettiamo tra le parentesi, mentre i valori di tali
-                 chiavi sono i valori delle variabili con i nomi corrispondenti
-                 alle stringhe inserite
+Route::prefix('admin')
+    ->name('admin.')
+    ->middleware('auth')
+    ->group(function () {
 
-        compact('firstName', 'lastName')
-         |                                     |
-         V                                     V
+    Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
 
-         [
-            'firstName' => $firstName,
-            'lastName' => $lastName,
-         ]
-    */
-
-    /*
-        dd: vuol dire dump and die, cioè fai il var_dump (più carino però)
-            e poi stoppa l'esecuzione
-    */
-    // dd(compact('firstName', 'lastName'));
-
-    return view('welcome', [
-        'firstName' => $firstName,
-        'lastName' => $lastName,
-    ]);
-    // return view('welcome', compact('firstName', 'lastName'));
 });
 
-Route::get('/chi-siamo', function () {
-    return view('subpages.about');
-});
-
-// Route::get(PERCORSO CON CUI ARRIVARE ALLA PAGINA, FUNZIONE DI CALLBACK CHE MI CREA LA RISPOSTA DA DARE ALL UTENTE)
+require __DIR__.'/auth.php';
--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+
 import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                // Compila il mio file scss in css (che verrà messo dentro public)
                 'resources/scss/app.scss',
-                'resources/js/app.js'
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
     resolve: {
         alias: {
-            // Creiamo un nome alternativo al percorso /resources/
             '~resources': '/resources/',
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap')
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
         }
     },
 });
